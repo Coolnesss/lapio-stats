7565060ce3003ff82acb194f1c83eb5dc3801e27
Chang <chang.rajani@gmail.com>
2015-07-10 01:17:11 +0300

    Added some validations. Made create submission form better

--- app/models/submission.rb
+++ app/models/submission.rb
@@ -2,6 +2,7 @@ class Submission < ActiveRecord::Base
   belongs_to :week
 
   validates :student_id, presence: true, length: {is: 9}
+  validate :points_is_less_than_max
 
   def self.search(search)
     where("student_id LIKE ?", "%#{search}%")
@@ -10,4 +11,9 @@ class Submission < ActiveRecord::Base
   def percentage
     ((self.points.to_f / self.week.max_points) * 100).round(2)
   end
+
+  private
+    def points_is_less_than_max
+      errors.add(:points, "should be less than max points") if points > self.week.max_points
+    end
 end
--- app/models/week.rb
+++ app/models/week.rb
@@ -1,3 +1,7 @@
 class Week < ActiveRecord::Base
   has_many :submissions
+
+  validates :name, presence: true
+  validates :max_points, presence: true
+
 end
