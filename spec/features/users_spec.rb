ae598feb4f619199a3cf888a15f93c9a5083e2c2
Chang <chang.rajani@gmail.com>
2015-08-27 22:18:09 +0300

    add test to make sure it works

---
+++ spec/features/users_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+describe "User" do
+
+  before :each do
+    FactoryGirl.create :user
+    sign_in(name:"joni", password:"paras")
+  end
+
+  it "can change password" do
+    visit root_path
+
+    click_link('Change password')
+    fill_in("Password", with: "parasta")
+    fill_in("Password confirmation", with: "parasta")
+
+    click_button('Submit')
+
+    expect(page).to have_content("User updated successfully")
+    click_link("Sign out")
+
+    expect(page).not_to have_content("joni signed in")
+    sign_in(name:"joni", password:"parasta")
+    expect(page).to have_content("joni signed in")
+  end
+end
