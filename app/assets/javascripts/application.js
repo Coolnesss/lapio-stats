2165e85bd0d0e9355af5c93c345a89dc77c42b82
Chang <chang.rajani@gmail.com>
2016-09-01 11:34:19 +0300

    Fix folding create

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,8 +11,7 @@
 // about supported directives.
 //
 //= require jquery
+//= require bootstrap-sprockets
 //= require jquery_ujs
 //= require turbolinks
-//= require bootstrap-sprockets
-//= require mluukkai
 //= require_tree .
