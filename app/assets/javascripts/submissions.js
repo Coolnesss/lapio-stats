0f9a4e1783769dc9a2ef5e6cf5195605cf7e485b
Irene <irene.nikkarinen@helsinki.fi>
2017-08-31 23:35:20 +0300

    Fix double jquery event on load

--- app/assets/javascripts/submissions.js
+++ app/assets/javascripts/submissions.js
@@ -1,4 +1,4 @@
-$(document).on('ready page:load turbolinks:load', function(){
+$(document).on('turbolinks:load', function(){
 
   $("#submissions").tablesorter({sortList: [[1,0]]});
 
@@ -32,7 +32,6 @@ $(document).on('ready page:load turbolinks:load', function(){
     var weekId = $("#dropdown").length
                     ? parseInt($("#dropdown").val())
                     : parseInt($("#submission-week-id").text())
-    $("#dropdown").unbind('change');
     $("#dropdown").on("change", onDropdownChange);
     $("#submission-points").val(0)
     for (let i = 1; i <= points[weekId]; i++) {
@@ -50,11 +49,9 @@ $(document).on('ready page:load turbolinks:load', function(){
   }
 
   function onCheckboxChecked(e) {
-    console.log(e)
     var checked = e.target.checked
     var diff = checked ? 1 : -1;
     $("#submission-points").val(parseInt($("#submission-points").val()) + diff)
-    console.info(parseInt($("#submission-points").val()) + diff)
   }
 });
 
