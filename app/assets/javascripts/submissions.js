adfc23e5d16115d3ab7906ea00254e1b351c1279
Irene <irene.nikkarinen@helsinki.fi>
2017-08-31 22:28:23 +0300

    Add marking points by checkboxes

--- app/assets/javascripts/submissions.js
+++ app/assets/javascripts/submissions.js
@@ -21,4 +21,39 @@ $(document).on('ready page:load turbolinks:load', function(){
       $elem.toggle(studentNumber.indexOf(filter) !== -1);
     });
   })
+
+  var points = {}
+  if (window.location.href.includes("submissions") || $("#dropdown").length) {
+    $.getJSON(window.location.origin + "/weeks.json", function (response) {
+      response.forEach(function (week) {
+        points[week.id] = week.max_points
+      })
+      onDropdownChange()
+    var weekId = $("#dropdown").length
+                    ? parseInt($("#dropdown").val())
+                    : parseInt($("#submission-week-id").text())
+    $("#dropdown").unbind('change');
+    $("#dropdown").on("change", onDropdownChange);
+    $("#submission-points").val(0)
+    for (let i = 1; i <= points[weekId]; i++) {
+      $('#checkbox-' + i).on("change", onCheckboxChecked);
+    }
+  })
+}
+
+  function onDropdownChange() {
+    $("#points-container").html('')
+    $("#submission-points").val(0)
+    for (let i = 1; i <= points[parseInt($("#dropdown").val())]; i++) {
+      $("#points-container").append('<input type="checkbox" id="checkbox-' + i + '"> Exercise ' + i +'</input><br>')
+    }
+  }
+
+  function onCheckboxChecked(e) {
+    var checked = e.target.checked
+    var diff = checked ? 1 : -1;
+    $("#submission-points").val(parseInt($("#submission-points").val()) + diff)
+    console.info(parseInt($("#submission-points").val()) + diff)
+  }
 });
+
